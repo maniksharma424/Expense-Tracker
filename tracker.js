a5002ae4ec0acc7aef83ef04aa5065daefd54d4e
MANIKSHARMA2000 <115614705+MANIKSHARMA2000@users.noreply.github.com>
2022-11-22 23:02:33 +0530

    v1.2

--- tracker.js
+++ tracker.js
@@ -23,11 +23,21 @@ function RotateEXP(){
     ExpCard.style.transform = 'rotateY(180deg)'
 }
 function AddIncome(){
+
+    
+    if(IncInputDescription.value === '' ){
+        IncInputDescription.placeholder = "Please add description"
+    }
+    if(IncInputAmount.value === '' ){
+        IncInputAmount.placeholder = "Please add amount"
+    }
+    else{
     arr.push({'amount':`${IncInputAmount.value}`,
-    'description':`${IncInputDescription.value}`})
+    'description':`${IncInputDescription.value}`,
+'type':`income`})
     renderlist(arr)
     
-    localStorage.setItem('entry',JSON.stringify(arr))
+    localStorage.setItem('listitems',JSON.stringify(arr))
     
 
     updateAdd(IncomeValue,parseInt(IncInputAmount.value))
@@ -36,11 +46,23 @@ function AddIncome(){
     IncInputAmount.value = ''
     IncInputDescription.value = ''
 
-}
+}}
 
 function AddExpense(){
-    arr.push({'amount':`${ExpInputAmount.value}`,
-    'description':`${ExpInputDescription.value}`})
+    if(ExpInputAmount.value ==='' && ExpInputDescription.value === '' ){
+        ExpInputAmount.placeholder = "Please add amount"
+        ExpInputDescription.placeholder = "Please add description"
+    }
+    if(ExpInputAmount.value === ''){
+        ExpInputAmount.placeholder = "Please add amount"
+        
+    }
+    if(ExpInputDescription.value === ''){
+        ExpInputDescription.placeholder = "Please add description"
+    }
+   else{arr.push({'amount':`${ExpInputAmount.value}`,
+    'description':`${ExpInputDescription.value}`,
+'type':`expense`})
 
     renderlist(arr)
 
@@ -52,7 +74,7 @@ function AddExpense(){
     ExpCard.style.transform = 'rotateY(0deg)'
     ExpInputDescription.value = ''
     ExpInputAmount.value = ''
-}
+}}
 function updateAdd(obj,number){
    let PrintValue =  parseInt(obj.textContent) + number
     if(obj.id === 'expense-value'){
@@ -72,7 +94,8 @@ function updateAdd(obj,number){
 let ItemFromLocalStorage = JSON.parse(localStorage.getItem('listitems'))
 
 if(ItemFromLocalStorage){
-    renderlist(ItemFromLocalStorage)
+    arr = ItemFromLocalStorage
+    renderlist(arr)
 }
 let IncomeMoneyLocalStorage = JSON.parse(localStorage.getItem('moneyincome'))
 let ExpenseMoneyLocalStorage = JSON.parse(localStorage.getItem('moneyexpense'))
@@ -88,14 +111,24 @@ function renderValue(obj,aray){
 }
 
 function renderlist(array){
-    let addhtml =''
+    let addhtml = ''
     for(let i=0; i<array.length; i++){
+        if(array[i].type === 'income'){
     addhtml = addhtml+ `
     <li id="li-el">
-        <div class="ulel-discription">${array[i].description}<h6>(${day} - ${month} - ${year})</h6></div>
+        <div class="ulel-discription">${array[i].description}<h6 style="color: hsl(31, 77%, 52%);" >(${day} - ${month} - ${year})</h6></div>
         <div class="ulel-amount">$${array[i].amount}</div>
         <div style="background-color: hsl(31, 77%, 52%);" class="ulel-color"></div>
     </li>`
+    }
+    if(array[i].type === 'expense'){
+        addhtml = addhtml+ `
+    <li id="li-el">
+        <div class="ulel-discription">${array[i].description}<h6 style="color: hsl(184, 100%, 22%);">(${day} - ${month} - ${year})</h6></div>
+        <div class="ulel-amount">$${array[i].amount}</div>
+        <div style="background-color: hsl(184, 100%, 22%);" class="ulel-color"></div>
+    </li>`
+    }
     }
     UlEl.innerHTML = addhtml
 }
